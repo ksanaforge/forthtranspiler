f2104c69a5c4128c03d4083d80506e0560ce8fe3
samsuanchen <samsuanchen@gmail.com>
2015-04-04 18:19:08 +0800

    add trace(true)

--- src/transpile.js
+++ src/transpile.js
@@ -1,9 +1,10 @@
+var tracing=0;
 var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 // forthCodes: forth source codes
-// runtime: 
-// inputfn: input file name
+//  runtime: 
+//  inputfn: input file name
 // outputfn: output file name
-	var lines=forthCodes, tokens=[], opCodes=[], defined={}, idx={};
+	var lines=forthCodes, tokens=[], opCodes=[], defined={}, iCol={};
 	var iLin=0, iCol=0, iTok=0, iOpCode=0;
 	var line='', token, opCode;
 	if(typeof(lines)=='string') lines=forthCodes.split(/\r?\n/);
@@ -15,8 +16,11 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 	}
 	var nextToken=function(){
 		token=undefined;
-		if(iTok<tokens.length)
+		if(iTok<tokens.length){
+			var i=iTok, j=iCol[i];
 			token=tokens[iTok++];
+			if(tracing) console.log('\tcol '+(j<10?'0':'')+j+' token '+i+': '+JSON.stringify(token));
+		}
 		return token;
 	}
 	var checkNextToken=function(){
@@ -25,31 +29,17 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 			token=tokens[iTok];
 		return token;
 	}
-	var nextOpCode=function(){
-		opCode=undefined;
-		if(iOpCode<opCodes.length)
-			opCode=opCodes[iOpCode++];
-		return opCode;
-	}
-	var checkNextOpCode=function(){
-		var opCode=undefined;
-		if(iOpCode<opCodes.length)
-			opCode=opCodes[iOpCode]; // 
-		return opCode;
-	}
-	////////////////////////////////////////////////////////////////////////////
-	// all the functions that can be pushed into opCodes
-	var doLit=function(n) {	/// doLit ( -- n ) compileOnly
+	var doLit=function(n) {	/// doLit ( -- n )
 		n=JSON.stringify(n);
-		var adv=1, checkNextOpc=checkNextOpCode();
-		if(checkNextOpc){
-			if 		   (checkNextOpc==dup		)
+		var adv=1, nextOpc=opCodes[iOpCode+1];
+		if(nextOpc){
+			if 		   (nextOpc==dup		)
 				codegen.push("stack.push("+n+");"), codegen.push("stack.push("+n+");");
-			else	if (checkNextOpc==plus		)
+			else	if (nextOpc==plus		)
 				codegen.push("stack[stack.length-1]+="+n+";");
-			else	if (checkNextOpc==minus	)
+			else	if (nextOpc==minus	)
 				codegen.push("stack[stack.length-1]-="+n+";");
-			else	if (checkNextOpc==multiply	)
+			else	if (nextOpc==multiply	)
 				codegen.push("stack[stack.length-1]*="+n+";");
 			else
 				codegen.push("stack.push("+n+");"), adv=0; /// no extra advance
@@ -57,47 +47,92 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 				codegen.push("stack.push("+n+");"), adv=0; /// no extra advance
 		return adv;
 	}
-	var dup=function() {				/// dup	( n -- n n )
+	//////////////////////////////////////////////////////////////////////////
+	// core words
+	//////////////////////////////////////////////////////////////////////////
+	var dup=function dup() { /// dup	( n -- n n )
 		codegen.push("stack.push(stack[stack.length-1]);");
 	}
-	var multiply=function() {			/// *	( a b -- a*b )
+	var multiply=function multiply() { /// *	( a b -- a*b )
 		codegen.push("stack.push(stack.pop()*stack.pop());");
 	}
-	var plus=function() {				/// + ( a b -- a+b )
+	var plus=function plus() { /// + ( a b -- a+b )
 		codegen.push("var tos=stack.pop();stack.push(stack.pop()+tos);");
 	}
-	var dot=function() {				/// .	( n -- )
+	var dot=function dot() { /// .	( n -- )
 		codegen.push("console.log(stack.pop());");
 	}
-	var minus=function() {				/// -	( a b -- a-b )
+	var minus=function minus() { /// -	( a b -- a-b )
 		codegen.push("var tos=stack.pop();stack.push(stack.pop()-tos);");
 	}
-	var doVal=function(name) {			/// doVal ( n <name> -- ) compileOnly
+	//////////////////////////////////////////////////////////////////////////
+	/// opCode constructing words 
+	//////////////////////////////////////////////////////////////////////////
+	var setValue=function(name) { /// setVal('v') ( n -- )
 		codegen.push("var "+name+"=stack.pop();")
 		return 1;
 	}
-	var getVal=function(name) {			/// 
+	var getValue=function(name) { /// v ( -- n )
 		codegen.push("stack.push("+name+");");
 		return 1;
 	}
-	var value=function(){ // value ( n <name> -- )
-		var name=nextToken();
-		if(name) { var x;
-			eval('x=function(){getVal("'+name+'")}')
-			defined[name]=x;
-		//	console.log('defined['+name+']:'+defined[name])
-			eval('x=function(){doVal("' +name+'")}')
-			opCodes.push( x );
-			addMapping(token), jsline++;
-		} else
-			throw 'need a name after value at line '+iLin+' column '+iCol;
+	var newName, xt; // globle variables
+	var setColon=function(name){ /// : <name> ( -- )
+		codegen.push('var '+name+'=function(){');
 	}
-	var words = { "dup"		: {xt:dup		,defining:0} /// dup	 ( n -- n n )
-				, "*"		: {xt:multiply	,defining:0} /// *	 ( a b -- a*b )
-				, "+"		: {xt:plus		,defining:0} /// +	 ( a b -- a+b )
-				, "."	 	: {xt:dot		,defining:0} /// .	 ( n -- )
-				, "-"		: {xt:minus		,defining:0} /// -	 ( a b -- a-b )
-				, "value"	: {xt:value		,defining:1} /// value ( n <name> -- )
+	var endColon=function(){ /// ; ( -- )
+		codegen.push('}');
+	}
+	var runColon=function(name){ /// <name>
+		codegen.push(name+"();");
+	}
+	//////////////////////////////////////////////////////////////////////////
+	/// defining words
+	//////////////////////////////////////////////////////////////////////////
+	var cmd='';
+	var value=function(){ /// value <newName> ( n -- )
+		newName=nextToken();
+		if(newName) {
+			cmd+=' '+newName;
+			eval('xt=function(){setValue("'+newName+'")}');
+			if(tracing) showOpInfo('setValue("'+newName+'")');
+			opCodes.push( xt ); // we define newName to setValue
+			addMapping('value '+newName), jsline++;
+			eval('xt=function(){getValue("'+newName+'")}');
+			defined[newName]=xt; // then use newName to getValue
+		//	console.log('defined['+newName+']:'+xt)
+		} else
+			throw 'need newName for "value" at line '+iLin+' column '+iCol;
+	}
+	var colon=function(){ /// : ( <name> -- )
+		newName=nextToken();
+		if(newName) {
+			cmd+=' '+newName;
+			eval('xt=function(){setColon("'+newName+'")}');
+			if(tracing) showOpInfo('setColon("'+newName+'")');
+			opCodes.push( xt ); // setColon to begin colon defintion
+			addMapping(': '+newName), jsline++;
+		} else
+			throw 'need newName for ":" at line '+iLin+' column '+iCol;
+	}
+	var semic=function(){ /// ; ( -- )
+		var xt;
+		var txt='xt=function(){endColon()}';
+		eval(txt)
+		if(tracing) showOpInfo('endColon()')
+		opCodes.push( xt ); // endColon to end colon defintion
+		addMapping(token), jsline++;
+		eval('newXt=function(){runColon("'+newName+'")}');
+		defined[newName]=newXt; // then use newName to runColon
+	}
+	var words = { "dup"		: {xt:dup		,defining:0} /// dup	( n -- n n )
+				, "*"		: {xt:multiply	,defining:0} /// *	 	( a b -- a*b )
+				, "+"		: {xt:plus		,defining:0} /// +	 	( a b -- a+b )
+				, "."	 	: {xt:dot		,defining:0} /// .	 	( n -- )
+				, "-"		: {xt:minus		,defining:0} /// -	 	( a b -- a-b )
+				, "value"	: {xt:value		,defining:1} /// value	( n <name> -- )
+				, ":"		: {xt:colon		,defining:1} /// :		( <name> -- )
+				, ";"		: {xt:semic		,defining:1} /// ;		( -- )
 				}
 	
 	var SourceMapGenerator=require("source-map").SourceMapGenerator;
@@ -118,110 +153,61 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 		  name: name
 		});
 	}
+	var showOpInfo=function(msg){
+		console.log('\t\t\t\topCode '+opCodes.length+': '+msg);
+	}
 	var forth2js=function(lines){
 		opCodes=[];
 		defined={};
+		if(tracing) console.log('\ntranspiling:');
 		for (var i=0;i<lines.length;i++) {
 			forthnline=i+1; 
 			line=lines[i];
-		//	console.log('forthnline:'+forthnline,'i:'+i,'line:'+JSON.stringify(line))
-			var idx={};
-			line.replace(/\S+/g,function(m,j){
-			//	console.log('idx['+JSON.stringify(m)+']:'+j)
-				idx[m]=j; });
+			if(tracing) console.log('\tline '+i+': '+JSON.stringify(line));
+			iCol=[];
+			iTok=0;
+			line.replace(/\S+/g,function(token,j){ iCol[iTok++]=j; }); // iCol for each token
 			iTok=0,tokens=line.split(/\s+/);
-		//	console.log(tokens.length+' tokens')
+			cmd='';
 			while(checkNextToken()!==undefined){
-				token=nextToken();
-				forthncol=idx[token];
-			//	console.log('forthncol:'+forthncol,'iTok:'+(iTok-1),'token:'+JSON.stringify(token))
-				var def=defined[token];
-				if (def) {
-				//	console.log('def:'+def)
-					opCodes.push(def);
+				forthncol=iCol[iTok]+1;
+				cmd=token=nextToken();
+				var xt=defined[token];
+				if (xt) {
+					if(tracing) showOpInfo(xt.toString().match(/\{(\S+)\}/)[1]);
+					opCodes.push(xt);
 					addMapping(token), jsline++;
 				} else {
 					var w=words[token];
 					if (w) {
-					//	console.log('w:'+w)
+						xt=w.xt;
 						if(w.defining)
-							w.xt();
+							xt(); // execute defining word directly
 						else {
-							opCodes.push(w.xt);
-							addMapping(token), jsline++;      //assuming only generate one js source line
+							if(tracing) showOpInfo(xt.toString().match(/function\s+(\S+)\s+/)[1]);
+							opCodes.push(xt);
+							addMapping(token), jsline++;	//assuming only generate one js source line
 						}
 					} else {
 						var n=(parseFloat(token));
 						if (isNaN(n)) {
 							var M=token.match(/^'(\S+)'$/);
 							if (M) {
-							//	console.log('string:'+JSON.stringify(M[1]))
-								opCodes.push(M[1]);
+								var str=M[1];
+								if(tracing) showOpInfo(JSON.stringify(str));
+								opCodes.push(str);
 								addMapping(token), jsline++;
 							} else {
 								throw "unknown word:"+token;
 							}
 						} else {
-						//	console.log('float:'+n)
+							if(tracing) showOpInfo(n)
 							opCodes.push(n);
 							addMapping(token), jsline++;
 						}
-					} // endif (w)
-				} // endif (def)
-			}/*
-			source.replace(/\S+/g,function(m,j){ idx[m]=j; }
-				var rest=source.substr(idx);
-				var M=rest.match(/(\S+)(\s+)?(\S+)?/)
-			//	if(source==='5 value five five') console.log('001 >>> idx:',idx,'rest:',rest);
-				if(hidden[idx]){
-				//	console.log('get hidden',m,'at',idx);
-					return;
-				}
-				forthncol=idx;
-				if(m=='value'){
-				//	console.log('002 >>> idx:',idx,'rest:',rest);
-					//opCodes.push('var '+M[3]+'=stack.pop()');
-					opCodes.push( setVal );
-					opCodes.push(M[3]);
-				//	addMapping(m);
-					var j=idx+(M[1]+M[2]).length;
-					values[M[3]]=j;
-				//	console.log('set hidden',M[3],'at',j);
-					hidden[j]=1;
-					jsline++;
-					return
-				}
-				var v=values[m];
-				if (v) {
-				//	console.log('003 >>> idx:',idx,'rest:',rest);
-					opCodes.push( getval );
-					opCodes.push( m );
-					//opCodes.push('stack.push('+m+')')
-				//	addMapping(m);
-					jsline++;
-				} else {
-					var w=words[m];
-					if (w) {
-						opCodes.push(w);
-						addMapping(m);
-						jsline++;      //assuming only generate one js source line
-					} else {
-						var n=(parseFloat(m));
-						if (isNaN(n)) {
-							var M=m.match(/'(\S+)'/);
-							if (M) {
-								opCodes.push(M[1]);
-								jsline++;
-							} else {
-								throw "unknown word "+m;
-							}
-						} else {
-							opCodes.push(n);
-							jsline++;
-						}
-					} // endif (w)
-				} // endif (v)
-			});*/
+					} // end if (w)
+				} // end if (def)
+			} // end while (checkNextToken()!==undefined)
 		}
 		return opCodes;
 	}
@@ -229,8 +215,10 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 	var codegen=[];                //generated javsacript code
 	var forthnline=0,forthncol=0;  //line and col of forth source code
 
-//	console.log('opCodes:',opCodes);
 	var opCodes=forth2js(lines);
+	for(var i=0;i<opCodes.length;i++){
+		var op=opCodes[i];
+	}
 	var i=0; 
 	while(i<opCodes.length){
 		var adv=0;
@@ -242,27 +230,39 @@ var transpilejs=function(forthCodes,runtime,inputfn,outputfn) {
 		if (adv) i+=adv;
 		i++;
 	}
-
-
 	return {jsCodes:codegen,sourcemap:sourcemap,opCodes:opCodes};
 }
 
 var runtimecode	=require("fs").readFileSync("./src/runtime.js","utf8");
 var transpile=function(forth) {
-	console.log('forthCode:',JSON.stringify(forth[0]));
+if(tracing) console.log('\nforthCode:',JSON.stringify(forth[0]));
 	var trans=transpilejs(forth,runtimecode,"test");
-	var code =	"(function(){					\n"
-			 +	"var stack=[];					\n"
-			 +	"var runtime={stack:stack};		\n"
-			 +	   trans.jsCodes.join("\n")  + "\n"
-			 +	"return runtime;				\n"
+	var code =	"(function(){"				+"\n"
+			 +	"var stack=[];"				+"\n"
+			 +	"var runtime={stack:stack};"+"\n"
+			 +	trans.jsCodes.join("\n")	+"\n"
+			 +	"return runtime;"			+"\n"
 			 +	"})()";
-	console.log('jsCode:\n',code);
+	var indent='\t';
+	var jsCode=trans.jsCodes.map(function(line,i){
+		if(line.match(/^\s*\}/)){
+			indent=indent.substr(0,indent.length-1);
+		}
+		var adjust='\t'+i+indent+line;
+		if(line.match(/\{\s*$/)){
+			indent+='\t'
+		}
+		return adjust;
+	}).join('\n');
+	if(tracing) console.log('jsCode:\n'+jsCode)
 	try {
 		var res=eval(code);
-		console.log('stack:',JSON.stringify(res.stack));
+		if(tracing) console.log('\nresult stack: '+JSON.stringify(res.stack)+'\n');
 	}
 	catch(e) { console.log(e) }
 	return res;
 }
-module.exports=transpile;
+var trace=function(flag){
+	tracing=flag;
+}
+module.exports={transpile:transpile,trace:trace};
--- test/test.js
+++ test/test.js
@@ -1,52 +1,70 @@
 var assert		=require("assert");
-var transpile	=require("../src/transpile.js");
+var Transpile	=require("../src/transpile.js");
+var transpile	=Transpile.transpile;
 
-describe(						"A.  test core words"
-	,function(){				/////////////////////
+Transpile.trace(true);
+describe(						"A.  test literals"
+	,function(){				///////////////////
 
-	it(								"A1.  test numbers"
-	,function(){assert.deepEqual(	///////////////////
-		transpile([					"3 5"
-		]).stack,					[3,5]
+	it(								"A1.  test integers ok"
+	,function(){assert.deepEqual(	//////////////////////
+		transpile([					"0 1 23 -4 56789"
+		]).stack,					[0,1,23,-4,56789]
 	)})
 
-	it(								"A2.  test + numbers"
+	it(								"A2.  test floats ok"
+	,function(){assert.deepEqual(	//////////////////////
+		transpile([					"0.1 -2.3 -4.5e6"
+		]).stack,					[0.1,-2.3,-4.5e6]
+	)})
+
+	it(								"A3.  test strings ok"
+	,function(){assert.deepEqual(	//////////////////////
+		transpile([					"'0.1' 'abc' 'deFghi'"
+		]).stack,					['0.1','abc','deFghi']
+	)})
+
+});
+
+describe(						"B.  test core words"
+	,function(){				/////////////////////
+
+	it(								"B1.  test + numbers ok"
 	,function(){assert.deepEqual(	/////////////////////
 		transpile([					"3 5 +"
 		]).stack,					[8]
 	)})
 
-	it(								"A3.  test - numbers"
+	it(								"B2.  test - numbers ok"
 	,function(){ assert.deepEqual(	/////////////////////
 		transpile([					"5 3 -"
 		]).stack,					[2]
 	)})
 
-	it(								"A4.  test string"
-	,function(){ assert.deepEqual(	//////////////////
-		transpile([					"'3'"
-		]).stack,					["3"]
-	)})
-
-	it(								"A5.  test + strings"
+	it(								"B3.  test + strings ok"
 	,function(){ assert.deepEqual(	/////////////////////
 		transpile([					"'3' 5 +"
 		]).stack,					["35"]
 	)})
 
-	it(								"A6.  test dup"
+	it(								"B4.  test dup * ok"
 	,function(){ assert.deepEqual(	///////////////
-		transpile([					"3 dup"
-		]).stack,					[3,3]
+		transpile([					"3.5 dup *"
+		]).stack,					[12.25]
 	)})
 
-	it(								"A7.  test value"
+});
+
+describe(						"C.  test defining words"
+	,function(){				/////////////////////
+
+	it(								"C1.  test value ok"
 	,function(){ assert.deepEqual(	/////////////////
 		transpile([					"5 value a 3 a +"
 		]).stack,					[8]
 	)})
 
-	it(								"A8.  test : ;"
+	it(								"C2.  test : ; ok"
 	,function(){ assert.deepEqual(	/////////////////
 		transpile([					": x 5 + ; 3 x"
 		]).stack,					[8]
